a7ca5e72a2045cdb096492a3a1d0ae4f702b7315
denisecodes <denise.chan112@gmail.com>
2023-10-07 21:20:22 +0100

    use weatherapi and display weather data on index.html

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
 
-@app.route('/find_weather', methods=['POST'])
+@app.route('/', methods=['POST'])
 def find_weather():
-    print("finding weather")
-    return render_template('index.html')
+    user_postcode = request.form['postcode']
+    parameters = {
+        "key": os.environ.get("API_KEY"),
+        "q": user_postcode,
+        "api": "no"
+    }
+    response = requests.get("http://api.weatherapi.com/v1/current.json", params=parameters)
+    print(f"User's postcode: {user_postcode}")
+    data = response.json()
+    location_data = data['location']
+    print(location_data)
+    current_data = data['current']
+    print(current_data)
+    return render_template('index.html', location=location_data, current=current_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
