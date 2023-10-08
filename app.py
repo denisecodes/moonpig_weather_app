28538a12df76c346f5d5b1d13878b1f5a5ffaa34
denisecodes <denise.chan112@gmail.com>
2023-10-08 18:32:20 +0100

    change post request route to /show_weather and render weather.html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
-import requests
+from flask import Flask, render_template, request, url_for
 import os
 from dotenv import load_dotenv
+import requests
 
 # Load environment variables from .env
 load_dotenv()
@@ -12,23 +12,23 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def find_weather():
-    user_postcode = request.form['postcode']
+@app.route('/show_weather', methods=['POST'])
+def show_weather():
+    user_input = request.form['search-bar']
     parameters = {
         "key": os.environ.get("API_KEY"),
-        "q": user_postcode,
+        "q": user_input,
         "api": "no"
     }
     response = requests.get("http://api.weatherapi.com/v1/current.json", params=parameters)
     print(response)
-    print(f"User's postcode: {user_postcode}")
+    print(f"User's input: {user_input}")
     data = response.json()
     location_data = data['location']
     print(location_data)
     current_data = data['current']
     print(current_data)
-    return render_template('index.html', location=location_data, current=current_data)
+    return render_template('weather.html', location=location_data, current=current_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
